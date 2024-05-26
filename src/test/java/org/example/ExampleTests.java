61f6514646e12a604a9d6981843dae5832f5dd9f
andreidiakov <andreidiakov2100@gmail.com>
2024-05-26 22:04:21 +0300

    update_tests

--- src/test/java/org/example/ExampleTests.java
+++ src/test/java/org/example/ExampleTests.java
@@ -1,14 +1,13 @@
 package org.example;
 import org.junit.jupiter.api.*;
 
-import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @TestInstance(TestInstance.Lifecycle.PER_CLASS)
-public class Тests {
+public class ExampleTests {
     private ArrayList<Long> numbers;
 
     @BeforeAll
@@ -16,28 +15,29 @@ public class Тests {
         numbers = NumberOperations.readNumbersFromFile("test_numbers.txt");
     }
 
-
-
     @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     @Test
     public void testMin() {
         assertEquals(Long.valueOf( 1), NumberOperations.getMin(numbers));
+        System.out.println("Тест на минимум пройден успешно");
     }
 
     @Test
     public void testMax() {
         assertEquals(Long.valueOf( 10000), NumberOperations.getMax(numbers));
+        System.out.println("Тест на максимум пройден успешно");
     }
 
     @Test
     public void testSum() {
         assertEquals(String.valueOf( 11431), NumberOperations.getSum(numbers));
+        System.out.println("Тест на сумму пройден успешно");
     }
 
     @Test
     public void testMult() {
-
         assertEquals("8114798039040000", NumberOperations.getMult(numbers));
+        System.out.println("Тест на произведение пройден успешно");
     }
 
     @Disabled
