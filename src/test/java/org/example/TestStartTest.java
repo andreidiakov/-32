9f604ebe0bda829ec3244b760ffc94cad729aaf5
andreidiakov <andreidiakov2100@gmail.com>
2024-05-26 22:52:52 +0300

    update

--- src/test/java/org/example/TestStartTest.java
+++ src/test/java/org/example/TestStartTest.java
@@ -1,41 +1,40 @@
 package org.example;
 import org.junit.jupiter.api.*;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+
 public class TestStartTest {
     private ArrayList<Long> numbers;
 
-    @BeforeAll
-    public void init() throws Exception {
-        numbers = NumberOperations.readNumbersFromFile("test_numbers.txt");
-    }
-
-    @Timeout(value = 200, unit = TimeUnit.MILLISECONDS)
     @Test
-    public void testMin() {
+    public void testMin() throws IOException {
+        numbers = NumberOperations.readNumbersFromFile("test_numbers.txt");
         assertEquals(Long.valueOf( 1), NumberOperations.getMin(numbers));
         System.out.println("Тест на минимум пройден успешно");
     }
 
     @Test
-    public void testMax() {
+    public void testMax() throws IOException {
+        numbers = NumberOperations.readNumbersFromFile("test_numbers.txt");
         assertEquals(Long.valueOf( 10000), NumberOperations.getMax(numbers));
         System.out.println("Тест на максимум пройден успешно");
     }
 
     @Test
-    public void testSum() {
+    public void testSum() throws IOException {
+        numbers = NumberOperations.readNumbersFromFile("test_numbers.txt");
         assertEquals(String.valueOf( 11431), NumberOperations.getSum(numbers));
         System.out.println("Тест на сумму пройден успешно");
     }
 
     @Test
-    public void testMult() {
+    public void testMult() throws IOException {
+        numbers = NumberOperations.readNumbersFromFile("test_numbers.txt");
         assertEquals("8114798039040000", NumberOperations.getMult(numbers));
         System.out.println("Тест на произведение пройден успешно");
     }
